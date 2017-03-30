9d545ecfb26ff68487af374dd48f323509728a2c
Liam Bryan <wbryan@protonmail.com>
2017-03-30 09:58:04 -0700

    I think this will work

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Submit from './components/Submit.js'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
+  render(){
+    return(
+      <div>
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        
+        <Submit />
       </div>
-    );
+    )
   }
 }
 
---
+++ src/components/Submit.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import superagent from 'superagent';
+
+class Submit extends Component {
+  constructor(props){
+    super(props);
+    this.state = {name: '', email: ''};
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.submit = this.submit.bind(this);
+  }
+
+  render() {
+    return (
+      <div className="">
+        <form onSubmit={this.submit}>
+          <input onChange={this.handleNameChange} type="text" name="name" placeholder="Enter Name" />
+          <br />
+          <input onChange={this.handleEmailChange} type="email" name="email" placeholder="Enter Email" />
+          <input type="submit" value="submit" />
+        </form>
+      </div>
+    );
+  }
+
+  submit(event){
+    event.preventDefault();
+    superagent.post('https://hooks.zapier.com/hooks/catch/2116348/1umb3d/')
+      .set('Content-Type', 'text/plain').send(JSON.stringify(Object.assign(this.state, { timestamp: new Date()})))
+      .end(function(err, res){
+        console.log(err, res);
+      });
+  }
+
+  handleNameChange(event){
+    this.setState({name: event.target.value});
+  }
+
+  handleEmailChange(event){
+    this.setState({email: event.target.value});
+  }
+}
+
+export default Submit;

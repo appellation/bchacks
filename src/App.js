c320acdd80628795b4bb44f530b6c1d358da8cf3
Franz Ferdinand <wbryan@protonmail.com>
2017-03-31 11:38:34 -0700

    Added Eventbrite widget
    Would like to disable the scroll bar on it

--- src/App.js
+++ src/App.js
@@ -6,16 +6,42 @@ import Submit from './components/Submit.js'
 class App extends Component {
   render(){
     return(
-      <div>
-        <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="App content">
+        <div className="box">
+          <div className="box">
+            <h1>BCHacks</h1>
+            <h2>May 27th-28th, 2017</h2>
+            <img src={logo} className="App-logo" alt="logo" />
+          </div>
+          <p className="App-intro">
+            To get signed up, enter your name and email address.
+          </p>
+
+          <Submit />
+
+          <div className="eventDiv">
+            <iframe src="//eventbrite.com/tickets-external?eid=33324768279&ref=etckt" frameborder="0" height="275" width="100%" vspace="0" hspace="0" marginheight="5" marginwidth="5" scrolling="auto" allowtransparency="true"></iframe>
+            <div className="eventDivTwo">
+              <a className="powered-by-eb" target="_blank" href="http://www.eventbrite.com/">Powered by Eventbrite</a>
+            </div>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        
-        <Submit />
+        <div className="box">
+          <h1>About</h1>
+          <hr />
+        </div>
+        <div className="box">
+        <h1>About</h1>
+          <hr />
+          </div>
+        <div className="box">
+        <h1>About</h1>
+          <hr />
+          </div>
+        <div className="box">
+          <h1>About</h1>
+          <hr />
+        </div>
       </div>
     )
   }

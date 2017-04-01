e76923ddbbd5ce5e5376932597c72d44561f9214
appellation <nelson.will@live.com>
2017-03-31 17:28:45 -0700

    add roboto, fix iframe attributes

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -5,7 +5,7 @@ class Register extends Component {
     return (
       <div className="eventDiv box box-back">
         <h3 className="box">Tickets</h3>
-        <iframe src="//eventbrite.com/tickets-external?eid=33324768279&ref=etckt" frameborder="0" height="275" width="100%" vspace="0" hspace="0" marginheight="5" marginwidth="5" scrolling="auto" allowtransparency="true"></iframe>
+        <iframe src="//eventbrite.com/tickets-external?eid=33324768279&amp;ref=etckt" frameBorder="0" height="275" width="100%" marginHeight="5" marginWidth="5" scrolling="auto" allowTransparency="true"></iframe>
         <div className="eventDivTwo">
           <a className="powered-by-eb" target="_blank" href="http://www.eventbrite.com/">Powered by Eventbrite</a>
         </div>
